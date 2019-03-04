692f31dc94a1220bc8a5e5b293853e1bb6925c8a
Salamander1012 <salmanfakhri2@gmail.com>
2019-03-04 00:48:49 -0500

    added firebase auth

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, jsonify, url_for, redirect
 from functools import wraps
 
+import firebase_admin
+from firebase_admin import credentials, auth
+
 app = Flask(__name__)
 
 db = []
 
+cred = credentials.Certificate('firebase_private_key.json')
+firebase_app = firebase_admin.initialize_app(cred)
+
 # This function is a decorator
-def login_required(f):
+def auth_required(f):
 	@wraps(f)
 	def verify_token(*args, **kwargs):
-		
-		# Verify firebase auth token here	
-		
+		# Verify firebase auth token here
+		try:	
+			id_token = request.args.get('token')
+			result = auth.verify_id_token(id_token)
+			print(result['name'] + " made a request")
+		except:
+			return jsonify({"error": "Bad token"})		
 		return f(*args, **kwargs)
 	return verify_token
 
@@ -24,7 +34,7 @@ def login():
     return "login"
 
 @app.route('/transactions', methods=['GET', 'POST'])
-@login_required
+@auth_required
 def transactions():
     if request.method == 'POST':
 		transaction = {
